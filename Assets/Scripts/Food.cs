7f1291699d37317c6fac17cca0da66641fb3a99a
wLabyr <sBum.Seo@gmail.com>
2017-01-18 03:32:54 +0900

    음식 내려옴

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour {
+	void Start() {
+		
+	}
+	
+	void Update() {
+		/* 좌표 이동 */
+		float endposition = GameObject.Find("BeltEnd").transform.position.y;
+		float position = transform.position.y - Time.deltaTime;
+		if(position > endposition) {
+			transform.position = new Vector3(transform.position.x, position, transform.position.z);
+		} else {
+			DestroyObject(gameObject);
+		}
+
+		/* 크기 확대 */
+		float scale = transform.localScale.x + Time.deltaTime / 100f;
+		transform.localScale = new Vector3(scale, scale, 1f);
+	}
+}
--- Assets/Scripts/Scenes/Stomach.cs
+++ Assets/Scripts/Scenes/Stomach.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class Stomach : MonoBehaviour {
 	void Start() {
-
+		InvokeRepeating("CreateFood", 1f, 1.5f);
 	}
 	
 	void Update() {
 		
+	}
+
+	void CreateFood() {
+		GameObject food = GameObject.Instantiate(GameObject.Find("BeltStart"));
+		string type;
+
+		// 이미지 랜덤 적용 (햄버거 or 샐러드)
+		switch(Random.Range(0, 3)) {
+			case 0:		type = "Hamburger";	break;
+			case 1:		type = "Chicken";	break;
+			default:	type = "Salad";		break;
+		}
+		food.GetComponent<SpriteRenderer>().sprite = GameObject.Find(type).GetComponent<SpriteRenderer>().sprite;
+		// Z값 설정
+		food.transform.position = new Vector3(food.transform.position.x, food.transform.position.y, -11);
+		// Food 컴포넌트 추가
+		food.AddComponent<Food>();
 	}
 }
